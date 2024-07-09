3f8c82b6a865386f7dfb30243e2f7a7344c31b0d
himayrobr <169061000+himayrobr@users.noreply.github.com>
2024-07-08 19:02:13 -0500

    feat: :sparkles: "primera parte lista"

---
+++ main.js
@@ -0,0 +1,41 @@
+class persona{
+    #edad;
+    #nombre;
+    #genero;
+    constructor(edad, nombre, genero){
+        this.#edad =edad;
+        this.#nombre =nombre;
+        this.#genero =genero;
+    }
+    set edad(edad){
+        this.#edad = edad;
+    }
+    get edad(){
+        return this.#edad;
+    }
+    set nombre(nombre){
+        this.#nombre = nombre;
+    }
+    get nombre(){
+        return this.#nombre;
+    }
+    set genero(genero){
+        this.#genero = genero;
+    }
+    get genero(){
+        return this.#genero;
+    }
+    saludar(){
+        return `hola, mi nombre es ${this.#nombre} tengo ${this.#edad} de edad y me soy de genero ${this.#genero}`;
+    }
+}
+
+let persona1= new persona (28, "sergio", "masculino");
+
+console.log(persona1.saludar());
+
+
+document.querySelector("#persona").innerHTML =/*html*/`
+    <h1>Nombre: <span>${persona1.edad}</span></h1>
+    <h2>Nombre: <span>${persona1.nombre}</span></h2>
+    <h2>Nombre: <span>${persona1.genero}</span></h2>`;
